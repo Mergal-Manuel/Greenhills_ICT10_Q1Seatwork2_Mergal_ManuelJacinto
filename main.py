d458c7a9ca8e558894f68c1c1f8273c18820f519
Mergal-Manuel <sup96445@gmail.com>
2025-09-08 19:46:55 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Data types in python
+from pyscript import display
+
+
+restaurant_name = 'Mergals Table' #string
+owner_name = 'Mergal' #string
+year_established = 2013 #integer
+popular_item_price = 139 #integer
+has_delivery = True #boolean
+product_names = ['bibimbap', 'Siomai rice', 'Siopao', 'Soopong noodles', 'Chow Mein'] #list
+business_hours = '3am-6pm' #string
+menu_prices = ['99 php', '49 php', '149 php', '179 php'] #list
+common_allergens = ['Wheat', 'Egg', 'Soy'] #list
+tax_rate = 0.05 #float
+
+display(f'Welcome to the official site of {restaurant_name}! This was founded {year_established} by {owner_name} for his love of oriental cuisine.', target='infotext')
+display(f'Hungry? here is a exclusive offer for exactly those who clicked the About Us page! Claim your {product_names[0]} for only {popular_item_price} PHP instead of {menu_prices[3]}!', target='infotext2')
