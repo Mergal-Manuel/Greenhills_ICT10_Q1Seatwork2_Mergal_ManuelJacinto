657265c9cda96b0378ef5f0f2183894fcc4ac7ce
Mergal-Manuel <sup96445@gmail.com>
2025-10-12 15:49:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ common_allergens = ['Wheat', 'Egg', 'Soy'] #list
 tax_rate = 0.05 #float
 
 display(f'Welcome to the official site of {restaurant_name}! This was founded {year_established} by {owner_name} for his love of oriental cuisine.', target='infotext')
-display(f'Hungry? here is a exclusive offer for exactly those who clicked the About Us page! Claim your {product_names[0]} for only {popular_item_price} PHP instead of {menu_prices[3]}!', target='infotext2')
+display(f'Hungry? here is a exclusive offer for exactly those who clicked the Home page! Claim your {product_names[0]} for only {popular_item_price} PHP instead of {menu_prices[3]} in the button above!', target='infotext2')
