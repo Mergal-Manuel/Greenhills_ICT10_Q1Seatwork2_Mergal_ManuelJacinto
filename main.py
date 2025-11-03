dea657cb9d980c4f5a238d26a3fe95e886a578e2
Mergal-Manuel <sup96445@gmail.com>
2025-11-03 14:18:14 +0800

    Add grade calculation functionality

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-#Data types in python
-from pyscript import display
+from pyscript import document, display
 
+def get_grades(e):
 
-restaurant_name = 'Mergals Table' #string
-owner_name = 'Mergal' #string
-year_established = 2013 #integer
-popular_item_price = 139 #integer
-has_delivery = True #boolean
-product_names = ['bibimbap', 'Siomai rice', 'Siopao', 'Soopong noodles', 'Chow Mein'] #list
-business_hours = '3am-6pm' #string
-menu_prices = ['99 php', '49 php', '149 php', '179 php'] #list
-common_allergens = ['Wheat', 'Egg', 'Soy'] #list
-tax_rate = 0.05 #float
+    document.getElementById('outputInfo').innerHTML = "   "
+    document.getElementById('outputMath').innerHTML = "   "
+    document.getElementById('outputSci').innerHTML = "   "
+    document.getElementById('outputEng').innerHTML = "   "
+    document.getElementById('outputICT').innerHTML = "   "
+    document.getElementById('outputFilo').innerHTML = "   "
+    document.getElementById('outputPE').innerHTML = "   "
+    document.getElementById('outputGradeAve').innerHTML = "   "
 
-display(f'Welcome to the official site of {restaurant_name}! This was founded {year_established} by {owner_name} for his love of oriental cuisine.', target='infotext')
-display(f'Hungry? here is a exclusive offer for exactly those who clicked the Home page! Claim your {product_names[0]} for only {popular_item_price} PHP instead of {menu_prices[3]} in the button above!', target='infotext2')
+    name = document.getElementById('name').value
+    section = document.getElementById('sect').value
+
+    document.getElementById('output1').innerHTML = "   "
+
+    display(f'{name} from {section}', target='outputInfo')
+
+    math = int(document.getElementById('Math').value)
+    sci = int(document.getElementById('Sci').value)
+    eng = int(document.getElementById('Eng').value)
+    ict = int(document.getElementById('ICT').value)
+    filo = int(document.getElementById('Filo').value)
+    pe = int(document.getElementById('PE').value)
+    
+    Subjects = [math, sci, eng, ict, filo, pe]
+    Units = (5, 5, 5, 2, 3, 1)
+
+    mathFinal = Subjects[0] * Units[0]
+    sciFinal = Subjects[1] * Units[1]
+    engFinal = Subjects[2] * Units[2]
+    ictFinal = Subjects[3] * Units[3]
+    filoFinal = Subjects[4] * Units[4]
+    peFinal = Subjects[5] * Units[5]
+
+    gen_ave = (mathFinal + sciFinal + engFinal + ictFinal + filoFinal + peFinal)/21
+
+    display(f'Math: {math}', target ='outputMath')
+    display(f'Science: {sci}', target ='outputSci')
+    display(f'English: {eng}', target ='outputEng')
+    display(f'ICT: {ict}', target ='outputICT')
+    display(f'Filipino: {filo}', target ='outputFilo')
+    display(f'PE: {pe}', target ='outputPE')
+    display(f'General Average: {gen_ave:.2f}', target='outputGradeAve')
+
